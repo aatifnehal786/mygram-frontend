568bb1521a83a275539261741e730fb2c81e64fa
aatifnehal786 <aatifnehal786@gmail.com>
2025-07-09 00:01:07 +0530

    .

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -132,9 +132,27 @@ const Chat = () => {
   };
 
   const startCall = async () => {
-    peerRef.current = new RTCPeerConnection({
-      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-    });
+   const iceServers = [
+  { urls: "stun:global.stun.twilio.com:3478" },
+  {
+    urls: "turn:global.turn.twilio.com:3478?transport=udp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  },
+  {
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  },
+  {
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  }
+];
+
+peerRef.current = new RTCPeerConnection({ iceServers });
+
 
     peerRef.current.onicecandidate = (e) => {
       if (e.candidate) {
@@ -183,15 +201,27 @@ const Chat = () => {
   const acceptCall = async () => {
     const { from, offer } = incomingCall;
 
-    peerRef.current = new RTCPeerConnection({
-      iceServers: [
-  { urls: 'stun:stun.l.google.com:19302' },
+    const iceServers = [
+  { urls: "stun:global.stun.twilio.com:3478" },
   {
-      urls: "stun:global.stun.twilio.com:3478"
-    }
-]
+    urls: "turn:global.turn.twilio.com:3478?transport=udp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  },
+  {
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  },
+  {
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+    credential: "tE2DajzSJwnsSbc123"
+  }
+];
 
-    });
+peerRef.current = new RTCPeerConnection({ iceServers });
+
 
     peerRef.current.onicecandidate = (e) => {
       if (e.candidate) {
