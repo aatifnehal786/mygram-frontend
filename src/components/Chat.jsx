6764e90803e21fb0a08302c168836077ac9abe45
aatifnehal786 <aatifnehal786@gmail.com>
2025-07-09 16:46:35 +0530

    .

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -132,24 +132,9 @@ const Chat = () => {
   };
 
   const startCall = async () => {
-   const iceServers = [
-  { urls: "stun:global.stun.twilio.com:3478" },
-  {
-    urls: "turn:global.turn.twilio.com:3478?transport=udp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  },
-  {
-    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  },
-  {
-    urls: "turn:global.turn.twilio.com:443?transport=tcp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  }
-];
+   const iceServers = [{ urls:["stun:stun.l.google.com:19302",
+                     "stun:global.stun.twilio.com:3478"]
+}]
 
 peerRef.current = new RTCPeerConnection({ iceServers });
 
@@ -201,24 +186,10 @@ peerRef.current = new RTCPeerConnection({ iceServers });
   const acceptCall = async () => {
     const { from, offer } = incomingCall;
 
-    const iceServers = [
-  { urls: "stun:global.stun.twilio.com:3478" },
-  {
-    urls: "turn:global.turn.twilio.com:3478?transport=udp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  },
-  {
-    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  },
-  {
-    urls: "turn:global.turn.twilio.com:443?transport=tcp",
-    username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-    credential: "tE2DajzSJwnsSbc123"
-  }
-];
+     const iceServers = [{ urls:["stun:stun.l.google.com:19302",
+                     "stun:global.stun.twilio.com:3478"]
+}]
+
 
 peerRef.current = new RTCPeerConnection({ iceServers });
 
