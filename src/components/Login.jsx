6719f81ed4154e720d626fecb03a57a34206256c
aatifnehal786 <aatifnehal786@gmail.com>
2025-09-06 17:03:57 +0530

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ export default function Login() {
     e.preventDefault();
     setIsLoading(true);
 
-    fetch("https://mygram-1-1nua.onrender.com/login", {
+    fetch(`${process.env.REACT_APP_API_URL}/login`, {
       method: "POST",
       body: JSON.stringify({ ...user, deviceId }), // include deviceId
       headers: { "Content-Type": "application/json" },
