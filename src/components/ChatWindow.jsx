a5fda3e45e887d6ea7e02100f6469b76991eacc6
aatifnehal786 <aatifnehal786@gmail.com>
2026-01-26 19:19:54 +0530

    .

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -8,6 +8,7 @@ import { clearUnread } from "../redux/slices/notificationSlice";
 import { setActiveChat, clearActiveChat } from "../redux/slices/chatSlice";
 import { FaVideo, FaEllipsisV } from 'react-icons/fa';
 import VideoCallManager from './VideoCallManager';
+import useVideoCallStore from "../store/VideoCallStore"
 
 
 const ChatWindow = ({ selectedUser, triggerForwardMode, messages, setMessages, onBack }) => {
