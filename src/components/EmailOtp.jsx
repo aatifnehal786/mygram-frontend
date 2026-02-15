b5749e80952a6fc3fa587a391aafdaa948e3302b
aatifnehal786 <aatifnehal786@gmail.com>
2026-02-15 14:23:53 +0530

    .

--- src/components/EmailOtp.jsx
+++ src/components/EmailOtp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef} from "react";
 import { Link } from "react-router-dom";
 import { toast,ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
