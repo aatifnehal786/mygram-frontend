423d8fc62bf359367434fb2358ef19d1ff815dba
aatifnehal786 <aatifnehal786@gmail.com>
2026-06-09 06:01:28 +0530

    .

--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -1,17 +1,14 @@
-// components/Private.jsx
-
 import { Navigate } from "react-router-dom";
 import useUserStore from "../store/useUserStore";
 
 export default function Private({ children }) {
-  // const { loggedUser } = useContext(UserContext);
-    const loggedUser = useUserStore.getState((state) => state.loggedUser);
-
+  const loggedUser = useUserStore(
+    (state) => state.loggedUser
+  );
 
   if (!loggedUser) {
-    // not logged in → redirect to login
     return <Navigate to="/login" replace />;
   }
 
-  return children; // render private component
+  return children;
 }
--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -1,17 +1,14 @@
-// components/PublicRoute.jsx
-// import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-// import { UserContext } from "../contexts/UserContext";
-import useUserStore from '../store/useUserStore'
+import useUserStore from "../store/useUserStore";
 
 export default function PublicRoute({ children }) {
-  
-   const loggedUser = useUserStore.getState((state) => state.loggedUser);
+  const loggedUser = useUserStore(
+    (state) => state.loggedUser
+  );
 
   if (loggedUser) {
-    // if logged in → redirect away from login/register
     return <Navigate to="/home" replace />;
   }
 
-  return children; // otherwise render the child component (e.g. Login)
+  return children;
 }
